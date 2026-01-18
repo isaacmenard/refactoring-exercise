a033aa5279276d6a4c3d11cb100dcec400805c7f
Isaac <i.menard@student.isartdigital.com>
2026-01-18 12:14:50 +0100

    refactor: migrated find_by to where

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -2,82 +2,16 @@ import polars as pl
 
 class FundingRaised:
   @staticmethod
-  def where(csv_path: str, options: dict):
+  def where(csv_path: str, options: dict, limit = -1):
     csv_data = FundingRaised._read_csv(csv_path)
+
     for key, value in options.items():
         csv_data = csv_data.filter(pl.col(key) == value)
+
+    if limit != -1:
+        csv_data = csv_data.head(limit)
+
     return csv_data.to_dicts()
-
-  @staticmethod
-  def find_by(csv_path: str, options = {}):
-    csv_data = FundingRaised._read_csv(csv_path)
-
-    if 'company_name' in options:
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    if 'city' in options:
-      for row in csv_data:
-        if row[4] == options['city']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    if 'state' in options:
-      for row in csv_data:
-        if row[5] == options['state']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    if 'round' in options:
-      for row in csv_data:
-        if row[9] == options['round']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    raise RecordNotFound
-
 
   def _read_csv(csv_path: str):
     with open(csv_path, "rb") as f:   # or "r" if you want text mode
--- python/test.py
+++ python/test.py
@@ -27,18 +27,16 @@ def test_where_returns_no_events():
   assert len(FundingRaised.where(csv_path, {'company_name': 'NotFacebook'})) == 0
 
 def test_find_by_event_by_company_name():
-  row = FundingRaised.find_by(csv_path, {'company_name': 'Facebook'})
-  keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-  values = ['facebook', 'Facebook', '450', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
-  for i in range(0, len(keys)):
-    assert row[keys[i]] == values[i]
+  row = FundingRaised.where(csv_path, {'company_name': 'Facebook'}, 1)
+  row_test = {'permalink': 'facebook', 'company_name': 'Facebook', 'number_employees': 450, 'category': 'web', 'city': 'Palo Alto', 'state': 'CA', 'funded_date': '1-Sep-04', 'raised_amount': 500000, 'raised_currency': 'USD', 'round': 'angel'}
+  assert len(row) == 1
+  assert row[0] == row_test
 
 def test_find_by_event_by_state():
-  row = FundingRaised.find_by(csv_path, {'state': 'CA'})
-  keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-  values = ['digg', 'Digg', '60', 'web', 'San Francisco', 'CA', '1-Dec-06', '8500000', 'USD', 'b']
-  for i in range(0, len(keys)):
-    assert row[keys[i]] == values[i]
+  row = FundingRaised.where(csv_path, {'state': 'CA'}, limit=1)
+  row_test = {'permalink': 'digg', 'company_name': 'Digg', 'number_employees': 60, 'category': 'web', 'city': 'San Francisco', 'state': 'CA', 'funded_date': '1-Dec-06', 'raised_amount': 8500000, 'raised_currency': 'USD', 'round': 'b'}
+  assert len(row) == 1
+  assert row[0] == row_test
 
 test_where_returns_events()
 test_where_returns_correct_keys()
