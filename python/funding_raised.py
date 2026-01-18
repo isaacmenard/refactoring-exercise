b823a6ec44f496ca4526c41b98875b27aaefa9c2
Isaac <i.menard@student.isartdigital.com>
2026-01-18 12:03:15 +0100

    refactor: 45min

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -1,71 +1,16 @@
-import csv
+import polars as pl
 
 class FundingRaised:
   @staticmethod
-  def where(options = {}):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
-
-    funding = []
-    if 'company_name' in options:
-      result = []
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          result.append(row)
-      csv_data = result
-
-    if 'city' in options:
-      result = []
-      for row in csv_data:
-        if row[4] == options['city']:
-          result.append(row)
-      csv_data = result
-
-    if 'state' in options:
-      result = []
-      for row in csv_data:
-        if row[5] == options['state']:
-          result.append(row)
-      csv_data = result
-
-    if 'round' in options:
-      result = []
-      for row in csv_data:
-        if row[9] == options['round']:
-          result.append(row)
-      csv_data = result
-
-    output = []
-    for row in csv_data:
-      mapped = {}
-      mapped['permalink'] = row[0]
-      mapped['company_name'] = row[1]
-      mapped['number_employees'] = row[2]
-      mapped['category'] = row[3]
-      mapped['city'] = row[4]
-      mapped['state'] = row[5]
-      mapped['funded_date'] = row[6]
-      mapped['raised_amount'] = row[7]
-      mapped['raised_currency'] = row[8]
-      mapped['round'] = row[9]
-      output.append(mapped)
-
-    return output
+  def where(csv_path: str, options: dict):
+    csv_data = FundingRaised._read_csv(csv_path)
+    for key, value in options.items():
+        csv_data = csv_data.filter(pl.col(key) == value)
+    return csv_data.to_dicts()
 
   @staticmethod
-  def find_by(options):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
+  def find_by(csv_path: str, options = {}):
+    csv_data = FundingRaised._read_csv(csv_path)
 
     if 'company_name' in options:
       for row in csv_data:
@@ -132,6 +77,11 @@ class FundingRaised:
           return mapped
 
     raise RecordNotFound
+
+
+  def _read_csv(csv_path: str):
+    with open(csv_path, "rb") as f:   # or "r" if you want text mode
+        return pl.read_csv(f, encoding="cp1252")
 
 class RecordNotFound(Exception):
   pass
--- python/test.py
+++ python/test.py
@@ -1,39 +1,40 @@
 from funding_raised import FundingRaised
+import polars as pl
+
+csv_path = "../startup_funding.csv"
 
 def test_where_returns_events():
-  assert len(FundingRaised.where({'company_name': 'Facebook'})) == 7
+  assert len(FundingRaised.where(csv_path, {'company_name': 'Facebook'})) == 7
 
 def test_where_returns_correct_keys():
-  row = FundingRaised.where({'company_name': 'Facebook'})[0]
-  keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-  values = ['facebook', 'Facebook', '450', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
-  for i in range(0, len(keys)):
-    assert row[keys[i]] == values[i]
+  row_test = {'permalink': 'facebook', 'company_name': 'Facebook', 'number_employees': 450, 'category': 'web', 'city': 'Palo Alto', 'state': 'CA', 'funded_date': '1-Sep-04', 'raised_amount': 500000, 'raised_currency': 'USD', 'round': 'angel'}
+  row = FundingRaised.where(csv_path, {'company_name': 'Facebook'})[0]
+  assert row == row_test
 
 def test_where_returns_events_by_city():
-  assert len(FundingRaised.where({'city': 'Tempe'})) == 3
+  assert len(FundingRaised.where(csv_path, {'city': 'Tempe'})) == 3
 
 def test_where_returns_events_by_state():
-  assert len(FundingRaised.where({'state': 'CA'})) == 873
+  assert len(FundingRaised.where(csv_path, {'state': 'CA'})) == 873
 
 def test_where_returns_events_by_company():
-  assert len(FundingRaised.where({'company_name': 'Facebook', 'round': 'a'})) == 1
+  assert len(FundingRaised.where(csv_path, {'company_name': 'Facebook', 'round': 'a'})) == 1
 
 def test_where_returns_events_by_type():
-  assert len(FundingRaised.where({'round': 'a'})) == 582
+  assert len(FundingRaised.where(csv_path, {'round': 'a'})) == 582
 
 def test_where_returns_no_events():
-  assert len(FundingRaised.where({'company_name': 'NotFacebook'})) == 0
+  assert len(FundingRaised.where(csv_path, {'company_name': 'NotFacebook'})) == 0
 
 def test_find_by_event_by_company_name():
-  row = FundingRaised.find_by({'company_name': 'Facebook'})
+  row = FundingRaised.find_by(csv_path, {'company_name': 'Facebook'})
   keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
   values = ['facebook', 'Facebook', '450', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
   for i in range(0, len(keys)):
     assert row[keys[i]] == values[i]
 
 def test_find_by_event_by_state():
-  row = FundingRaised.find_by({'state': 'CA'})
+  row = FundingRaised.find_by(csv_path, {'state': 'CA'})
   keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
   values = ['digg', 'Digg', '60', 'web', 'San Francisco', 'CA', '1-Dec-06', '8500000', 'USD', 'b']
   for i in range(0, len(keys)):
